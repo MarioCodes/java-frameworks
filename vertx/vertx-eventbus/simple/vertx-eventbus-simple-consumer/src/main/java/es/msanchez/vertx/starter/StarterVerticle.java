371d95930c592fa7e4569f2fa093f98276fde331
Mario Codes <mariocodes1993@gmail.com>
2017-12-01 20:39:14 +0100

    vertx-eventbus: Simple String
    Working example.
    Changed string address to enum.

--- vertx/vertx-eventbus/simple/vertx-eventbus-simple-consumer/src/main/java/es/msanchez/vertx/starter/StarterVerticle.java
+++ vertx/vertx-eventbus/simple/vertx-eventbus-simple-consumer/src/main/java/es/msanchez/vertx/starter/StarterVerticle.java
@@ -1,10 +1,19 @@
 package es.msanchez.vertx.starter;
 
+import es.msanchez.vertx.address.EventBusAddresses;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.EventBus;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class StarterVerticle extends AbstractVerticle {
 	@Override
-	public void start() throws Exception {
-		super.start();
+	public void start() {
+		EventBus bus = vertx.eventBus();
+
+		bus.consumer(EventBusAddresses.STRING_ADDRESS.toString(), handler -> {
+			String message = handler.body().toString();
+			log.info("Received message {}", message);
+		});
 	}
 }

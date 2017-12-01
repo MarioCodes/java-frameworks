eee09b1e60aa923f71faafe7595aba51be0fb230
Mario Codes <mariocodes1993@gmail.com>
2017-12-01 21:35:10 +0100

    vertx-eventbus: Simple String
    Added spring into producer .

--- vertx/vertx-eventbus/simple/vertx-eventbus-simple-producer/src/main/java/es/msanchez/vertx/starter/StarterVerticle.java
+++ vertx/vertx-eventbus/simple/vertx-eventbus-simple-producer/src/main/java/es/msanchez/vertx/starter/StarterVerticle.java
@@ -1,16 +1,26 @@
 package es.msanchez.vertx.starter;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import es.msanchez.vertx.address.EventBusAddresses;
+import es.msanchez.vertx.config.SpringConfig;
+import es.msanchez.vertx.utilities.SpringRegister;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.EventBus;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class StarterVerticle extends AbstractVerticle {
+
+	private AnnotationConfigApplicationContext context;
+
+	private SpringRegister register = new SpringRegister();
+
 	@Override
 	public void start() {
-		EventBus bus = vertx.eventBus();
+		context = register.iniSpringConfig(SpringConfig.class);
 
+		EventBus bus = context.getBean(EventBus.class);
 		vertx.setPeriodic(1000, event -> {
 			bus.send(EventBusAddresses.STRING_ADDRESS.toString(), "producer");
 			log.info("Sent message {}", "producer");

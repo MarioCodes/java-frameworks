d49909007b5147c0c6425f8c444ec9a2bf173e79
Mario Codes <mariocodes1993@gmail.com>
2017-12-01 21:41:44 +0100

    vertx-eventbus: Simple String
    Integrated spring into consumer.

--- vertx/vertx-eventbus/simple/vertx-eventbus-simple-consumer/src/main/java/es/msanchez/vertx/handler/MessageHandler.java
+++ vertx/vertx-eventbus/simple/vertx-eventbus-simple-consumer/src/main/java/es/msanchez/vertx/handler/MessageHandler.java
@@ -1,10 +1,13 @@
 package es.msanchez.vertx.handler;
 
+import org.springframework.stereotype.Component;
+
 import io.vertx.core.Handler;
 import io.vertx.core.eventbus.Message;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
 public class MessageHandler implements Handler<Message<String>> {
 
 	@Override

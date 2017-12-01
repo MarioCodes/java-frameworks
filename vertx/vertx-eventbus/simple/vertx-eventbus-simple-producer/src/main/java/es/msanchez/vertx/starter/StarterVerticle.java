5abc93f91d300e537559c3ed3033da17862e444f
Mario Codes <mariocodes1993@gmail.com>
2017-12-01 20:27:41 +0100

    vertx-eventbus: Simple String
    Structure complete, created main verticles for both projects.

---
+++ vertx/vertx-eventbus/simple/vertx-eventbus-simple-producer/src/main/java/es/msanchez/vertx/starter/StarterVerticle.java
@@ -0,0 +1,10 @@
+package es.msanchez.vertx.starter;
+
+import io.vertx.core.AbstractVerticle;
+
+public class StarterVerticle extends AbstractVerticle {
+	@Override
+	public void start() throws Exception {
+		super.start();
+	}
+}

3112692d412ea8865dcf0aade30944a06657a83f
Mario Codes Sánchez <m.sanchez@neusta.de>
2018-03-20 15:35:01 +0100

    spring: created module's skeleton

---
+++ spring/read-only-properties/src/main/java/es/msanchez/frameworks/spring/properties/Starter.java
@@ -0,0 +1,12 @@
+package es.msanchez.frameworks.spring.properties;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Starter {
+
+	public static void main(String[] args) {
+		log.info("Main started.");
+	}
+
+}

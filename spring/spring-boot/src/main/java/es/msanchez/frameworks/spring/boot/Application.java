a72ee80612a3a732779cefc396277cad24745788
Mario Codes Sánchez <m.sanchez@neusta.de>
2018-03-21 09:40:11 +0100

    spring: creating spring-boot-web-app

---
+++ spring/spring-boot/src/main/java/es/msanchez/frameworks/spring/boot/Application.java
@@ -0,0 +1,36 @@
+package es.msanchez.frameworks.spring.boot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @see "https://spring.io/guides/gs/spring-boot/"
+ */
+@Slf4j
+@SpringBootApplication
+public class Application {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner commandLineRunner(final ApplicationContext context) {
+    return args -> {
+      log.info("Beans provided by Spring Boot:");
+      final List<String> beanNames = Arrays.asList(context.getBeanDefinitionNames());
+
+      beanNames.stream().forEach(bean -> log.debug("Bean name '{}'", bean));
+    };
+
+  }
+
+}

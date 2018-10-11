cfb2a2883cbf383517996afbddff08b53b63db29
Mario Codes Sánchez <m.sanchez@neusta.de>
2018-10-11 09:17:03 +0200

    vertx-refactor: cleaned simple eventbus example

--- vertx/vertx-eventbus/simple/vertx-eventbus-simple-consumer/src/main/java/es/msanchez/vertx/handler/MessageHandler.java
+++ vertx/vertx-eventbus/simple/vertx-eventbus-simple-consumer/src/main/java/es/msanchez/vertx/handler/MessageHandler.java
@@ -1,18 +1,17 @@
 package es.msanchez.vertx.handler;
 
-import org.springframework.stereotype.Component;
-
 import io.vertx.core.Handler;
 import io.vertx.core.eventbus.Message;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class MessageHandler implements Handler<Message<String>> {
 
 	@Override
-	public void handle(Message<String> event) {
-		log.info("Received message {}", event.body().toString());
+	public void handle(final Message<String> event) {
+		log.info("Received message: {} from through EventBus", event.body());
 	}
 
 }

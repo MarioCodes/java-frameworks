4447871f8318e5faf8958307f7f329c251392185
Mario Codes <mariocodes1993@gmail.com>
2019-06-07 13:00:58 +0200

    docker-compose: created basic java app
    docker-compose: trying to get a running java app

---
+++ docker/dockerfile/my-java-app/app/src/main/java/es/msanchez/frameworks/java/spring/boot/Application.java
@@ -0,0 +1,32 @@
+package es.msanchez.frameworks.java.spring.boot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class Application {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner commandLineRunner(final ApplicationContext context) {
+    return args -> {
+      log.info("Beans provided by Spring Boot:");
+      final List<String> beanNames = Arrays.asList(context.getBeanDefinitionNames());
+      beanNames.stream().forEach(bean -> log.debug("Bean name '{}'", bean));
+    };
+
+  }
+
+}

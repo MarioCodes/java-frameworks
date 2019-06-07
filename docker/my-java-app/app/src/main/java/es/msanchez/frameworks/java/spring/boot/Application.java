10c737d9c280674b29d16e4d339d62b886a25f35
Mario Codes <mariocodes1993@gmail.com>
2019-06-07 15:44:23 +0200

    docker-compose: fixed spring DI

--- docker/my-java-app/app/src/main/java/es/msanchez/frameworks/java/spring/boot/Application.java
+++ docker/my-java-app/app/src/main/java/es/msanchez/frameworks/java/spring/boot/Application.java
@@ -1,34 +1,32 @@
 package es.msanchez.frameworks.java.spring.boot;
 
-import java.util.Arrays;
-import java.util.List;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
-  public static void main(final String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-  @Bean
-  public CommandLineRunner commandLineRunner(final ApplicationContext context) {
-    return args -> {
-      log.info("Beans provided by Spring Boot:");
-      final List<String> beanNames = Arrays.asList(context.getBeanDefinitionNames());
-      beanNames.forEach(bean -> log.debug("Bean name '{}'", bean));
-    };
+    @Bean
+    public CommandLineRunner commandLineRunner(final ApplicationContext context) {
+        return args -> {
+            log.info("Beans provided by Spring Boot:");
+            final List<String> beanNames = Arrays.asList(context.getBeanDefinitionNames());
+            beanNames.forEach(bean -> log.debug("Bean name '{}'", bean));
+        };
 
-  }
+    }
 
 }
--- docker/my-java-app/app/src/main/java/es/msanchez/frameworks/java/spring/boot/controller/HelloController.java
+++ docker/my-java-app/app/src/main/java/es/msanchez/frameworks/java/spring/boot/controller/HelloController.java
@@ -1,14 +1,16 @@
 package es.msanchez.frameworks.java.spring.boot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/my-java-app")
 public class HelloController {
 
-  @RequestMapping("/hello-world")
-  public String index() {
-    return "Hello world from Docker!";
-  }
+    @GetMapping("hello-world")
+    public String index() {
+        return "Hello world from Docker!";
+    }
 
 }
